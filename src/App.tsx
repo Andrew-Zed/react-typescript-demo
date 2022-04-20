2ebdd7e415b6f31c5523a645fba69cd587c1897a
Andrew-Zed <andrew.awadyke@gmail.com>
2022-04-20 01:41:51 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
+import { click } from '@testing-library/user-event/dist/click';
 import './App.css';
+import { Button } from './components/Button';
 import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
-  const personName ={
+  const personName = {
     first: 'Bruce',
     last: 'Wayne',
-  }
+  };
 
   const nameList = [
     {
       first: 'Bruce',
-      last: 'Wayne'
+      last: 'Wayne',
     },
     {
       first: 'Clark',
-      last: 'Kent'
+      last: 'Kent',
     },
     {
       first: 'Princess',
-      last: 'Diana'
-    }
-  ]
+      last: 'Diana',
+    },
+  ];
 
   return (
-    <div className='App'>
-      <Greet name='Magneto' messageCount={10} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+    <div className="App">
+      <div>
+        <Greet name="Magneto" messageCount={10} isLoggedIn={false} />
+        <Person name={personName} />
+        <PersonList names={nameList} />
+        <Status status="loading" />
+        <Heading>Placeholder text</Heading>
+        <Oscar>
+          <Heading>Oscar goes to Dicaprio</Heading>
+        </Oscar>
+        <Greet name="Andrew" isLoggedIn={false} />
+      </div>
+
+      <div className="button">
+        <Button
+          handleClick={(event, id) => {
+            console.log('Button clicked', event, id);
+          }}
+        />
+        <Input value='' handleChange={(event) => console.log(event)} />
+      </div>
+      <Container styles={{ border: '1px solid black', padding: '1rem' }}/>
     </div>
   );
 }
